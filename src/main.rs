ec1f011c94be219f01b9dcc69c15b7b6d6c68d6c
Bitcoin Eagle <github.bz24ml@bitcoineagle.dev>
2025-01-19 14:55:57 +0700

    Update launch.json, Cargo.lock, Cargo.toml, and 1 more file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,51 @@
-fn main() {
-    println!("Hello, world!");
+use bitcoincore_rpc::{
+    bitcoin::{Amount, BlockHash, Denomination},
+    bitcoincore_rpc_json::GetBlockHeaderResult,
+    json::{GetBlockTemplateCapabilities, GetBlockTemplateModes, GetBlockTemplateRules},
+    Client, RpcApi,
+};
+
+fn main() -> anyhow::Result<()> {
+    run()
+}
+
+fn run() -> anyhow::Result<()> {
+    let c = Client::new(
+        "localhost:8332",
+        bitcoincore_rpc::Auth::UserPass("rust-utxos".into(), "o4ka4wx3i0wxar0bec2w1sm9h".into()),
+    )?;
+    // let h = c.get_best_block_hash()?;
+    // println!("best block hash: {:?}\n", h);
+    // let m = c.get_mempool_info()?;
+    // println!("mempool info: {:?}\n", m);
+    // let m = c.get_raw_mempool()?;
+    // let m_v = c.get_raw_mempool_verbose()?;
+    let template = c.get_block_template(
+        GetBlockTemplateModes::Template,
+        &[
+            GetBlockTemplateRules::SegWit,
+            GetBlockTemplateRules::Csv,
+            GetBlockTemplateRules::Taproot,
+        ],
+        &[],
+    )?;
+    let weight_limit = template.weight_limit as usize;
+    let weight_half = weight_limit / 2;
+    let mut high_priority = None::<f64>;
+    let mut weight_acc = 0_usize;
+    for tx in template.transactions {
+        weight_acc += tx.weight;
+        if weight_half <= weight_acc {
+            high_priority = Some((tx.fee.to_sat() * 4) as f64 / tx.weight as f64);
+            break;
+        }
+    }
+    println!("high priority: {:?}\n", high_priority);
+    // c.send_raw_transaction(tx)
+    // println!("template: {:?}\n", t);
+    // let t_0 = t.transactions.first();
+    // println!("first tx: {:?}\n", t_0);
+    // let t_l = t.transactions.last();
+    // println!("last tx: {:?}\n", t_l);
+    Ok(())
 }
